5072e134d38473ddc5ee954fb5b4b1e75d80237d
Debanjan2204 <debanjan.clg@gmail.com>
2025-03-14 21:44:52 +0530

    Updated db connection settings

--- src/main/java/com/Connection/ConnectionUtil.java
+++ src/main/java/com/Connection/ConnectionUtil.java
@@ -11,24 +11,35 @@ public class ConnectionUtil {
 
     public static Connection getAdminConnection() throws SQLException {
     	try {
-			Class.forName("org.sqlite.JDBC");
+//			Class.forName("org.sqlite.JDBC");
+    		Class.forName("org.postgresql.Driver");
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		String ADMIN_DB_URL = "jdbc:sqlite:D:\\PROGRAMS X86 X64\\SQLITE\\Admin.db";
-        adminconn=DriverManager.getConnection(ADMIN_DB_URL);
+//		String ADMIN_DB_URL = "jdbc:sqlite:D:\\PROGRAMS X86 X64\\SQLITE\\Admin.db";
+    	String ADMIN_DB_URL = "jdbc:postgresql://postgres.railway.internal:5432/railway";
+    	String username = "postgres";
+    	String password = "mxpLNbLdAqNjIVUfnvVSjGxACBkWfULr"; 
+        adminconn=DriverManager.getConnection(ADMIN_DB_URL,username,password);
         return adminconn;
     }
 
     public static Connection CreatingConnectionCustDB() throws SQLException {
     	try {
-			Class.forName("org.sqlite.JDBC");
+//			Class.forName("org.sqlite.JDBC");
+    		Class.forName("org.postgresql.Driver");
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    	String CUSTOMER_DB_URL = "jdbc:sqlite:D:\\PROGRAMS X86 X64\\SQLITE\\Customer.db";
+//    	String CUSTOMER_DB_URL = "jdbc:sqlite:D:\\PROGRAMS X86 X64\\SQLITE\\Customer.db";
+//    	String CUSTOMER_DB_URL = "jdbc:postgresql://postgres.railway.internal:5432/{DATABASE_NAME}";
+    	String CUSTOMER_DB_URL = "jdbc:postgresql://postgres.railway.internal:5432/railway";
+    	String username = "postgres";
+    	String password = "mxpLNbLdAqNjIVUfnvVSjGxACBkWfULr"; // Make sure it's correct
+
+    	Connection conn = DriverManager.getConnection(CUSTOMER_DB_URL, username, password);
         conn= DriverManager.getConnection(CUSTOMER_DB_URL);
         return conn;
     }
